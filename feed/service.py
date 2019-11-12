73e48402ebbbfa81b9dc562bb10e9f5999f9356c
Rory McStay <rory@rorymcstay.com>
2019-11-12 21:29:25 +0000

    initial commit

---
+++ feed/service.py
@@ -0,0 +1,39 @@
+import sys
+import time
+import json
+import requests as r
+import logging
+from flask_classy import FlaskView
+
+
+class Service(FlaskView):
+
+    def healthCheck(self):
+        return "ok"
+
+
+class Client:
+    max_attempts = 10
+    wait = 10
+
+    def __init__(self, name, attempts=0, **params):
+        try:
+            hc = r.get("http://{host}:{port}/{api_prefix}/healthCheck")
+            if hc.status_code is 200:
+                self.health = True
+            if hc.status_code is 404:
+                logging.info(f'health check for {name}/{params.get("api_prefix")} was not found')
+            else:
+                logging.info("")
+                self.health = False
+        except Exception as e:
+            if attempts < self.max_attempts:
+                time.sleep(self.wait)
+                logging.warning(f'could not connect to {name}, trying again {self.max_attempts - attempts} more times')
+                self.__init__(name, **params)
+            else:
+                logging.error(f'could not connect to {name}. Giving up. \nParameters were:  \n{json.dumps(params, indent=4)}')
+                sys.exit()
+
+
+
---
+++ feed/settings.py
@@ -0,0 +1,53 @@
+import os
+
+kafka_params = {
+    "bootstrap_servers": [os.getenv("KAFKA_ADDRESS", "localhost:29092")],
+}
+
+browser_params = {
+    "port": os.getenv("BROWSER_PORT", 4444),
+    "host": os.getenv("BROWSER_CONTAINER_HOST", None),
+    "image": os.getenv('BROWSER_IMAGE', 'selenium/standalone-chrome:3.141.59')
+}
+
+routing_params = {
+    "host": os.getenv("ROUTER_HOST", "localhost"),
+    "port": os.getenv("FLASK_PORT", 5002),
+    "api_prefix": "routingcontroller"
+}
+
+nanny_params = {
+    "host": os.getenv("NANNY_HOST", "localhost"),
+    "port": os.getenv("FLASK_PORT", 5003),
+    "api_prefix": "containercontroller",
+    "params_manager": "parametercontroller"
+}
+
+hazelcast_params = {
+    "host": os.getenv("HAZELCAST_HOST", "localhost"), "port": os.getenv("HAZELCAST_PORT", 5701)
+}
+
+mongo_params = {
+    "host": os.getenv("MONGO_HOST", "localhost:27017"),
+    "username": os.getenv("MONGO_USER", "root"),
+    "password": os.getenv("MONGO_PASS", "root"),
+    "serverSelectionTimeoutMS": 5
+}
+
+
+database_parameters = {
+    "host": os.getenv("DATABASE_HOST", "localhost"),
+    "port": os.getenv("DATABASE_PORT", 5432),
+    "database": os.getenv("DATABASE_NAME", "postgres"),
+    "user": os.getenv("DATABASE_USER", "postgres"),
+    "password": os.getenv("DATABASE_PASS", "postgres"),
+}
+
+
+
+class BrowserConstants:
+    CONTAINER_TIMEOUT = int(os.getenv('CONTAINER_TIMEOUT', 10))
+    CONTAINER_SUCCESS = 'Selenium Server is up and running on port'
+    CONTAINER_QUIT = "Shutdown complete"
+    client_connect = 'wd/hub'
+    worker_timeout = int(os.getenv('WORKER_TIMEOUT', '3'))
