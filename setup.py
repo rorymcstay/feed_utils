0e9aac445fcfba639b594c01825208675cef0066
Rory McStay <rory@rorymcstay.com>
2020-03-27 22:42:04 +0000

    bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="feed_utils",
-    version="0.1.14",
+    version="0.1.17",
     author="Rory McStay",
     author_email="rory@rorymcstay.com",
     description="Common code for feed",
