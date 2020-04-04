756c576647fb48ee53ad0251569dd953f7e562dc
Rory McStay <rory@rorymcstay.com>
2020-04-04 18:37:56 +0100

    removing calls to nanny for container

--- feed/crawling.py
+++ feed/crawling.py
@@ -34,40 +34,23 @@ class BrowserService:
     retry_attempts = 10
 
     def __init__(self, attempts=0,
-                 getContainerUrl="http://{host}:{port}/{api_prefix}/getContainer".format(**nanny_params)):
         """
         Request a port of the nanny service and then start a webdriver session
         :param attempts: will recursively try to get a container, do not populate
         """
-
-        port = requests.get(getContainerUrl)
         self.getContainerUrl = getContainerUrl
         self.driver_url = ''
         try:
-            if not 100 < port.status_code < 400:
-                logging.warning(f'nanny returned with {port.status_code}, response was {port.text}')
-            else:
-                logging.info(f'connecting to container on port {port.text}')
-                self.port = port.text
-                if not all(char.isdigit() for char in self.port):
-                    logging.error(f'Nanny container did not return a valid port number {port.text}')
-                    raise CrawlerException(port.text)
-                # if no browser params specified then running in docker
-                # => different host
-                if browser_params['host'] is None:
-                    url = f'http://worker-{self.port}:{browser_params["internal_port"]}/wd/hub'
-                    logging.info(f'browser host is not set, using {url}')
-                else:
-                    # otherwise always localhost
-                    url = f'http://{browser_params["host"]}:{self.port}/wd/hub'
-                    logging.info(f'browser host is set, using {url}')
-                self.driver_url = url
-                logging.info(f'Starting remote webdriver with {self.driver_url}')
-                self.startWebdriverSession()
+            url = f'http://{browser_params["host"]}:{self.port}/wd/hub'
+            logging.info(f'browser host is set, using {url}')
+            self.driver_url = url
+            logging.info(f'Starting remote webdriver with {self.driver_url}')
+            self.startWebdriverSession()
         except Exception as e:
+            # TODO clean up exception handling
             traceback.print_exc()
             if attempts < self.retry_attempts:
-                logging.warning(f'error getting container: {port.text}')
+                logging.warning(f'error starting webdriver session : {port.text}')
                 sleep(self.retry_wait)
                 BrowserService.__init__(self, attempts=attempts + 1)
             else:
