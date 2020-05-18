79454593f19216b5fc3c738ca365810eb9836793
Rory McStay <rory@rorymcstay.com>
2020-05-18 02:19:52 +0100

    fixes

--- feed/chainsessions.py
+++ feed/chainsessions.py
@@ -41,7 +41,7 @@ class ChainSession(SessionInterface):
             logging.info(f'starting session for name=[{name.get("name")}]')
             chainSession = self._open_session(name=name.get('name'))
 
-        chainSession.update({'chain_db': self._client[os.getenv('SESSION_DATABASE', 'sessions')],
+        chainSession.update({'chain_db': self._client[os.getenv('CHAIN_DB', 'sessions')],
                              'chainDefinitions': self._chaindefinitions})
         return chainSession
 
