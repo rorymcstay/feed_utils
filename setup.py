40a0e9dba10c7e548e632cd990e3a9c0180bf74d
Rory McStay <rory@rorymcstay.com>
2020-06-13 02:52:50 +0100

    bump ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="feed_utils",
-    version="0.7.2",
+    version="0.7.3",
     author="Rory McStay",
     author_email="rory@rorymcstay.com",
     description="Common code for feed",
